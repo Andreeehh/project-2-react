6fe528ff524d2edbe1b544e6f7dba94f7a173a70
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-03-16 13:13:23 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const eventFn = () => {
+  console.log('h1 clicado');
+};
 
 function App() {
-  const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
+  const [counter2, setCounter2] = useState(0);
 
-  const handleClick = () => {
-    setReverse(!reverse);
-  };
+  //componentDidUpdate - executa toda vez que o component atualiza
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
 
-  const handleIncrement = () => {
-    setCounter((prevCounter) => prevCounter + 1);
-  };
+  //componentDidMount - executa 1x, passando array vazia de segundo parametro
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn);
+    //compontWillUmount - limpeza
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventFn);
+    };
+  }, []);
+
+  //Com dependência - executa toda vez q a dependência mudar
+  useEffect(() => {
+    console.log('contador mudou para', counter);
+    // setCounter(counter + 1); causa loop infinito
+  }, [counter]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <h1>Contador: {counter}</h1>
-        <button type="button" onClick={handleClick}>
-          Reverse {reverseClass}
-        </button>
-        <button type="button" onClick={handleIncrement}>
-          Increment
-        </button>
-      </header>
+      <p>Teste 1</p>
+      <h1>
+        Contador1: {counter} Contador2: {counter2}
+      </h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>+</button>
     </div>
   );
 }
