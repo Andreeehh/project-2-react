2a45189d8645127d46fa94a21b684ffc9ca4dce7
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-03-16 13:34:00 -0300

    useCallBack para evitar rerenderizar componentes que nao mudam, e functions que nao deveriam mudar

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
+import P from 'prop-types';
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-const eventFn = () => {
-  console.log('h1 clicado');
+const Button = React.memo(function Button({ incrementButton }) {
+  //React.memo para nao re renderizar um component que nao mudou
+  return <button onClick={() => incrementButton(10)}>+</button>;
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
 };
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
-  //componentDidUpdate - executa toda vez que o component atualiza
-  useEffect(() => {
-    console.log('componentDidUpdate');
-  });
-
-  //componentDidMount - executa 1x, passando array vazia de segundo parametro
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-    //compontWillUmount - limpeza
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
+  //salva a função na memória e nao re renderiza quando o counter mudar e re renderizar o app
+  const incrementCounter = useCallback((num) => {
+    //utiliza o setConter com prevCounter em vez do próprio counter e previne que a função mude, mantendo a mesma na memória
+    setCounter((c) => c + num);
   }, []);
-
-  //Com dependência - executa toda vez q a dependência mudar
-  useEffect(() => {
-    console.log('contador mudou para', counter);
-    // setCounter(counter + 1); causa loop infinito
-  }, [counter]);
 
   return (
     <div className="App">
       <p>Teste 1</p>
-      <h1>
-        Contador1: {counter} Contador2: {counter2}
-      </h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter2(counter2 + 1)}>+</button>
+      <h1>Contador1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
