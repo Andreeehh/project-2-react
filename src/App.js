f17a9d3d8a4a23813d681115a88d7a7a9be8f393
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-03-16 13:40:31 -0300

    useMemo para substituir React.memo no component Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import P from 'prop-types';
 import './App.css';
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 
-const Button = React.memo(function Button({ incrementButton }) {
-  //React.memo para nao re renderizar um component que nao mudou
+const Button = ({ incrementButton }) => {
+  console.log('filho renderizou');
   return <button onClick={() => incrementButton(10)}>+</button>;
-});
+};
 
 Button.propTypes = {
   incrementButton: P.func,
@@ -13,18 +13,23 @@ Button.propTypes = {
 
 function App() {
   const [counter, setCounter] = useState(0);
-
+  console.log('pai renderizou');
   //salva a função na memória e nao re renderiza quando o counter mudar e re renderizar o app
   const incrementCounter = useCallback((num) => {
     //utiliza o setConter com prevCounter em vez do próprio counter e previne que a função mude, mantendo a mesma na memória
     setCounter((c) => c + num);
   }, []);
 
+  const btn = useMemo(() => {
+    //useMemo = React.memo
+    return <Button incrementButton={incrementCounter} />;
+  }, [incrementCounter]);
+
   return (
     <div className="App">
       <p>Teste 1</p>
       <h1>Contador1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+      {btn}
     </div>
   );
 }
