c8c33694c9617ffa8682a06532c9abadbd748b5f
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-03-18 12:11:36 -0300

    Usando context como state global para evitar passar parametros para filhos

--- src/App.js
+++ src/App.js
@@ -1,73 +1,12 @@
-import P from 'prop-types';
-import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
-
-const Post = ({ post, handleClick }) => {
-  console.log('filho renderizou');
-  return (
-    <div key={post.id} className="post">
-      <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
-      <p onClick={() => handleClick(post.body)}>{post.body}</p>
-    </div>
-  );
-};
-
-Post.propTypes = {
-  post: P.shape({
-    id: P.number,
-    title: P.string,
-    body: P.string,
-  }),
-  handleClick: P.func,
-};
+import { AppContext } from './contexts/AppContext';
+import { Div } from './components/Div';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [value, setValue] = useState('');
-  const input = useRef(null);
-  const count = useRef(0);
-  console.log('pai renderizou!', count.current);
-
-  //Component did mount
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((r) => r.json())
-      .then((r) => setPosts(r));
-  }, []);
-
-  useEffect(() => {
-    input.current.focus();
-  }, [value]);
-
-  useEffect(() => {
-    count.current++;
-  });
-
-  const handleClick = (value) => {
-    setValue(value);
-  };
-
   return (
-    <div className="App">
-      <h1>Renderizou: {count.current}x</h1>
-      <p>
-        <input
-          ref={input}
-          type="search"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </p>
-      {useMemo(() => {
-        return (
-          posts.length > 0 &&
-          posts.map((post) => {
-            return <Post key={post.id} post={post} handleClick={handleClick} />;
-          })
-        );
-      }, [posts])}
-      {posts.length <= 0 && <p>Ainda nao existem posts.</p>}
-    </div>
+    <AppContext>
+      <Div></Div>
+    </AppContext>
   );
 }
 
