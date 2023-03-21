55c486873f53d8f8bc2b4273bd8a6ee33b126764
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-03-21 10:34:39 -0300

    useReducer para executar ações ao manipular um state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div';
+
+const globalState = {
+  title: 'O título do contexto',
+  body: 'o body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda com', action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      console.log('chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
   return (
-    <AppContext>
-      <Div></Div>
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('pt-BR'),
+          })
+        }
+      >
+        Muda
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Inverter</button>
+    </div>
   );
 }
 
